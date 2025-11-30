b28245ec0b46e6b3c6b3eb3ff912bcfb14ea2b1d
AbdulBasit0909 <232477@students.au.edu.pk>
2025-11-30 21:22:38 +0500

    Updated main.jsx with new Google OAuth ID and fixed sw.js

--- EventPulse-MERN-Project-main/client/src/main.jsx
+++ EventPulse-MERN-Project-main/client/src/main.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { GoogleOAuthProvider } from '@react-oauth/google'; // ✅ Import Provider
+import { GoogleOAuthProvider } from '@react-oauth/google'; 
 
-// 👇 PASTE YOUR GOOGLE CLIENT ID INSIDE THESE QUOTES 👇
-const GOOGLE_ID = "281397897114-35k0g11sp3sqttotdni7qvtiidifrump.apps.googleusercontent.com"; 
+const GOOGLE_ID = "593598799714-bvqj4db4190lbuhbu1scnduuplpop0da.apps.googleusercontent.com";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
